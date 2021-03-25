f10bdc3e8938b270949fbd4b82179ee3062fde32
pcmueller <pmuellerleile@gmail.com>
2021-03-24 21:18:21 -0600

    Add remaining Round class methods

--- src/Round.js
+++ src/Round.js
@@ -1,11 +1,49 @@
+const Turn = require("./Turn");
+
 class Round {
-  constructor(cards = []) {
-    this.deck = cards;
-    this.currentProperty = this.deck[0];
+  constructor(cardSet = []) {
+    this.deck = cardSet.cards;
+    this.currentCard = this.deck[0];
     this.turns = 0;
     this.incorrectGuesses = [];
-    this.percentCorrect = 0;f
+    this.percentCorrect = 0;
   }
+
+  returnCurrentCard() {
+    return this.currentCard;
+  }
+
+  takeTurn(guess) {
+    let turn = new Turn(guess, this.currentCard);
+    let status = turn.evaluateGuess();
+
+    if (!status) {
+      this.incorrectGuesses.push(this.currentCard.id);
+    }
+
+    this.turns++;
+    this.calculatePercentCorrect();
+    this.currentCard = this.deck[this.currentCard.id++];
+
+    if (this.turns === this.deck.length) {
+      return turn.giveFeedback() + "\n" + this.endRound();
+    } else {
+      return turn.giveFeedback();
+    }
+  }
+
+  calculatePercentCorrect() {
+    this.percentCorrect = (((this.turns - this.incorrectGuesses.length) / this.turns) * 100);
+    
+    return this.percentCorrect;
+  }
+
+  endRound() {
+    if (this.turns === this.deck.length) {
+      return `** Round over! ** You answered ${this.percentCorrect}% of the questions correctly!`;
+    }
+  }
+
 }
 
 module.exports = Round;
