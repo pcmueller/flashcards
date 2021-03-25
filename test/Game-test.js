7b8a3b70d578e7f890e53c8bfc1c2372f7ae871a
pcmueller <pmuellerleile@gmail.com>
2021-03-24 23:04:31 -0600

    Write additional Game tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -3,8 +3,6 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-// const Turn = require('../src/Turn');
-// const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Game', function() {
@@ -42,4 +40,15 @@ describe('Game', function() {
     expect(game.currentDeck.cards[0].correctAnswer).to.equal('object');
   });
 
+  it.skip('should be able to instantiate new round and assign as value of current round', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2,'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    const deck = new Deck([card1, card2, card3]);
+    const game = new Game();
+
+    game.start(deck);
+
+    expect(game.currentRound.deck.length).to.equal(3);
+  });
 });
